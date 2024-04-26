13262fbe383dcab446d74673f38f997856ba59ae
ghuali <guaghuali@gmail.com>
2024-04-26 09:03:03 +0100

    correction

--- ejemploExcepciones.py
+++ ejemploExcepciones.py
@@ -1,10 +1,11 @@
 class EjemploExcepciones:
     #ZeroDivisionError
-    def zeroDivisionError(self, *, num:int, den:int)->int:
+    def zeroDivisionError(self, *, num: int, den: int):
         if (den == 0):
             raise ZeroDivisionError
-        
         return num // den
+    
+
 
     #ValueError
     def valueError(self):
