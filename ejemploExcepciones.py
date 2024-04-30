5d51fe8275cd706ca1a9fcd649d18bc8e8b481ad
ghuali <148547419+ghuali@users.noreply.github.com>
2024-04-30 12:26:25 +0100

    index ejemploExcepciones.py

--- ejemploExcepciones.py
+++ ejemploExcepciones.py
@@ -31,6 +31,11 @@ class EjemploExcepciones:
 
     #IndexError
     def indexError(self):
+        lista = [1, 2, 3, 4, 5]
+        x = 0
+        while x < len(lista):
+            print(lista[x])
+            x += 1
         raise IndexError
 
     #KeyboardInterrupt
