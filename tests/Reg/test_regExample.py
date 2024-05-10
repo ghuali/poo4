98c61cd3cbb4b4bf75f0baec52b65ed7517a931d
ghuali <guaghuali@gmail.com>
2024-05-10 12:18:06 +0100

    Creado Reg en test

---
+++ tests/Reg/test_regExample.py
@@ -0,0 +1,12 @@
+import pytest
+from Reg.regExample import RegExample
+
+def test_vocales():
+    assert RegExample.buscar("Luna una cosa oso") == ["una", "oso"]
+
+def test_url():
+    assert RegExample.validURL("google") == False
+    assert RegExample.validURL("https://www.google.es") == True
+    assert RegExample.validURL("http://127.0.0.1") == True
+    assert RegExample.validURL("http://dominio.com/carpeta/archivo.ext") == True
+    assert RegExample.validURL("https://consuma.api/carpeta/archivo.ext#atr=2&uno=5") == True
